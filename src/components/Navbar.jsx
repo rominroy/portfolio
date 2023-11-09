c03e9bbdb40f7b01bfa10696e5e1952b195475e4
Romin Roy <romin.roy3@gmail.com>
2023-11-09 14:09:01 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,11 +67,11 @@ export const Navbar = () => {
         <div className='hidden md:flex fixed flex-col top-[35%] left-0'>
             <ul>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500'>
-                    <a className='flex justify-between items-center w-full text-gray-300 'href="/">
+                    <a className='flex justify-between items-center w-full text-gray-300 'href="https://linkedin.com/in/romin-roy-">
                         Linkedin<FaLinkedin size={30} /></a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-                    <a className='flex justify-between items-center w-full text-gray-300 'href="/">
+                    <a className='flex justify-between items-center w-full text-gray-300 'href="https://github.com/rominroy">
                         GitHub<FaGithub size={30} /></a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
